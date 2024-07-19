04fbfbef003166711d223a2eab391341932c1139
SPECTREG9 <saiber094@gmail.com>
2024-07-19 16:09:27 +0700

    แบบฝึกหัด

--- Ep.1/index.ts
+++ Ep.1/index.ts
@@ -8,3 +8,12 @@ let ต้องการกลับบ้าน: boolean = true
 console.log (firstName)
 console.log (y)
 console.log (ต้องการกลับบ้าน)
+
+const a: number = 4564
+const b: number = 4574
+
+console.log (a === b)
+
+console.log (a > b)
+
+console.log (a <= b)
