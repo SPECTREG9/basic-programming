cd1ccce4236b003d2cc8ec443b9f3982609294d5
SPECTREG9 <saiber094@gmail.com>
2024-07-19 15:32:36 +0700

    เรียนประกาศตัวแปร

---
+++ Ep.1/index.ts
@@ -0,0 +1,10 @@
+const firstName: string = 'จิรพัฒน์ ทานนท์'
+
+let y: Number = 10
+y = 20
+
+let ต้องการกลับบ้าน: boolean = true
+
+console.log (firstName)
+console.log (y)
+console.log (ต้องการกลับบ้าน)
