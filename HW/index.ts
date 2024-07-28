1f1da8e1ac8bbb97bedd0c094dab6ee627446dc8
SPECTREG9 <saiber094@gmail.com>
2024-07-28 23:34:16 +0700

    HW

--- HW/index.ts
+++ HW/index.ts
@@ -2,15 +2,15 @@ function bmi(weight: number, height: number) {
     let bmi: number = (weight / (height * height))
 
 if (bmi > 30.0) {
-    return 'Dangerous obesity'
+    return 'dangerous Obesity'
 } else if (bmi > 25.0) {
     return 'Obesity'
 } else if (bmi > 23.0) {
-    return 'Over weight' 
+    return 'over Weight' 
 } else if (bmi > 18.5) {
-    return 'Slim'
+    return 'slim'
 } else 
-    return 'lower weight'
+    return 'lower Weight'
 } 
 
 console.log(75/(1.76**2))
